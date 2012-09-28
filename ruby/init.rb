3ed795db2ed4b89c836c25c97a78da327ea82970
Chris McKnight <cmckni3@gmail.com>
2012-09-28 16:25:02 -0500

    Update ruby/init.rb

--- ruby/init.rb
+++ ruby/init.rb
@@ -6,10 +6,8 @@ trashy.init
 while true do
   time = Time.now.utc
   if time.wednesday? and time.hour == 4
-    puts "Sending"
     trashy.send_email
   else
-    puts "sleeping"
     sleep(30*60)
   end
 end
